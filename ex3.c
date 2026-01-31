0e22e360dcdf49e479c096828b0ee99438769292
Maya <ranbashusha@eng.tau.ac.il>
2026-01-31 16:02:32 +0200

    ex3.c done

--- ex3.c
+++ ex3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "fixed_point.h"
-
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
     if (argc != 6) {
@@ -10,7 +10,13 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    // TODO
+    int16_t x = (int16_t)atoi(argv[1]);
+    int16_t a = (int16_t)atoi(argv[2]);
+    int16_t b = (int16_t)atoi(argv[3]);
+    int16_t c = (int16_t)atoi(argv[4]);
+    int16_t q = (int16_t)atoi(argv[5]);
+
+    eval_poly_ax2_minus_bx_plus_c_fixed(x, a, b, c, q);
 
     return 0;
 }
